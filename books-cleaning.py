2d35d5dc26048c672b835ddef55a05185f264df7
Kelly <kellyegorman@gmail.com>
2024-07-09 22:07:53 -0400

    add to descriptive stats and reorganize files

--- books-cleaning.py
+++ books-cleaning.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
-bookData = pd.read_csv('books.csv')
-ratingsData = pd.read_csv('ratings.csv')
-usersData = pd.read_csv('users.csv')
+bookData = pd.read_csv('data/Books.csv')
+ratingsData = pd.read_csv('data/Ratings.csv')
+usersData = pd.read_csv('data/Users.csv')
 
 mergedISBN = pd.merge(bookData, ratingsData, how='inner', left_on='ISBN', right_on='ISBN')
 mergedUser = pd.merge(mergedISBN, usersData, how='inner', left_on='User-ID', right_on='User-ID')
 
-mergedUser.to_csv('mergedData.csv', sep='\t')
+mergedUser.to_csv('data/mergedData.csv', sep='\t')
 columnsDrop = ['Image-URL-S', 'Image-URL-M', 'Image-URL-L']
 merged = mergedUser.drop(columns=columnsDrop, axis=1)
 merged = merged.dropna()
-merged.to_csv('totalData.csv', sep='\t')
+merged.to_csv('data/totalData.csv', sep='\t')
 
 print(merged.head())
